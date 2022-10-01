6a2b1c8468926b2a0ad7ebbdc5c193d15a1ccd82
Jem Cope <thejemmeh@gmail.com>
2022-10-01 11:07:28 -0700

    Mole goes up and stops

--- OnteMinuteGameJam/Assets/Moles/BasicMole/UpDown.cs
+++ OnteMinuteGameJam/Assets/Moles/BasicMole/UpDown.cs
@@ -5,26 +5,38 @@ using UnityEngine;
 public class UpDown : MonoBehaviour
 {
     public float moleTimeUp = 1f;
-    public float moleSpeed = .000001f;
+    public float moleSpeed = .00001f;
 
     void Update()
     {
-        StartCoroutine(moleGoesUp(moleTimeUp));
+        //StartCoroutine(moleGoesUp(moleTimeUp));
+
+
+        moleGoesUp(moleTimeUp);
+
     }
 
-
-    IEnumerator moleGoesUp(float timeUp)
+    void moleGoesUp(float timeUp)
     {
-        float travelPercent = 0f;
-
-        Vector3 startPosition = transform.position;
-        Vector3 endPosition = new Vector3(transform.position.x, -0.051f, transform.position.z);
-
-        while (travelPercent < 1f) {
-            travelPercent += Time.deltaTime * moleSpeed;
-            transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
-            yield return new WaitForEndOfFrame();
+        if ( transform.position.y <= -0.051f)
+        {
+            transform.Translate(0, (.1f * Time.deltaTime), 0); // This works but it doesn't stop at a certain point
         }
     }
+
+
+    //IEnumerator moleGoesUp(float timeUp)
+    //{
+    //    float travelPercent = 0f;
+
+    //    Vector3 startPosition = transform.position;
+    //    Vector3 endPosition = new Vector3(transform.position.x, -0.051f, transform.position.z);
+
+    //    while (travelPercent < 1f) {
+    //        travelPercent += Time.deltaTime * moleSpeed;
+    //        transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+    //        yield return new WaitForEndOfFrame();
+    //    }
+    //}
 
 }
