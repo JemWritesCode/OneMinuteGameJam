fd6be35abcd331f45acd414c85cbb5f45bdba477
Jem Cope <thejemmeh@gmail.com>
2022-10-01 20:56:12 -0700

    Created the Object pool for the pumpkin moles

--- OnteMinuteGameJam/Assets/Moles/BasicMole/UpDown.cs
+++ OnteMinuteGameJam/Assets/Moles/BasicMole/UpDown.cs
@@ -15,47 +15,18 @@ public class UpDown : MonoBehaviour
     public float molePopupDuration = 5f;
     public float molePopupWait = 1f;
 
-    private void Start() {
-      var originalY = transform.position.y;
 
-      DOTween.Sequence()
-        .AppendInterval(molePopupDelay)
-        .Append(transform.DOMoveY(molePopupTargetY, molePopupDuration))
-        .AppendInterval(molePopupWait)
-        .Append(transform.DOMoveY(originalY, molePopupDuration))
-        .SetTarget(gameObject);
+    public void moleGoesUpAndDown(float molePopupWait)
+    {
+        //jemtodo : maybe add particle effects of vines growing or dirt moving when the pumpkin goes up and down
+
+        var originalY = transform.position.y;
+
+        DOTween.Sequence()
+          .AppendInterval(molePopupDelay)
+          .Append(transform.DOMoveY(molePopupTargetY, molePopupDuration))
+          .AppendInterval(molePopupWait)
+          .Append(transform.DOMoveY(originalY, molePopupDuration))
+          .SetTarget(gameObject);
     }
-
-  //void Update()
-  //  {
-  //      //StartCoroutine(moleGoesUp(moleTimeUp));
-
-
-  //      moleGoesUp(moleTimeUp);
-
-  //  }
-
-  //  void moleGoesUp(float timeUp)
-  //  {
-  //      if ( transform.position.y <= -0.051f)
-  //      {
-  //          transform.Translate(0, (.1f * Time.deltaTime), 0); // This works but it doesn't stop at a certain point
-  //      }
-  //  }
-
-
-    //IEnumerator moleGoesUp(float timeUp)
-    //{
-    //    float travelPercent = 0f;
-
-    //    Vector3 startPosition = transform.position;
-    //    Vector3 endPosition = new Vector3(transform.position.x, -0.051f, transform.position.z);
-
-    //    while (travelPercent < 1f) {
-    //        travelPercent += Time.deltaTime * moleSpeed;
-    //        transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
-    //        yield return new WaitForEndOfFrame();
-    //    }
-    //}
-
 }
---
+++ OnteMinuteGameJam/Assets/Moles/MoleManager.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoleManager : MonoBehaviour
+{
+    // for all the pumpkins in the mole list
+    // pick a random pumpkin
+    // make that random pumpkin go up and down
+
+    // nactually I need spawn ocoordinates that roughly work for all the different things that we might spawn instead
+    // and then spawn the pumpkin right there on the fly and it goes up. This will also mean less objects on the screen at once, we just spawn the pumpkins as needed
+
+    // spawner grid
+    //spawn pumpkins at random intervals
+    // there's different color pumpkins and there's a bomb pumkin that could be the pumpkin that you try to avoid
+
+    // i could rearrange the level so it's more of a square, zoom out the camera  bi tand make it spawn on coorindates instead of bespoke positioning 
+
+    // you make an object pool and you just change the position of the pumpkins each time you enable it
+    // //and it'll also reset it? or maybe hve to do the resetting manually?
+
+    [SerializeField] GameObject basicMole;
+    [SerializeField] [Range(0, 50)] int poolSize = 5;
+    [SerializeField] [Range(0.1f, 30f)] float spawnTimeInterval = 1f;
+
+    GameObject[] molePool;
+
+    void Start()
+    {
+        PopulatePool();
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private void PopulatePool()
+    {
+        molePool = new GameObject[poolSize];
+
+        for (int i = 0; i < molePool.Length; i++)
+        {
+            molePool[i] = Instantiate(basicMole, transform);
+            molePool[i].SetActive(false);
+        }
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (true) //jem todo til the timer runs out?
+        {
+            EnableObjectInPool();
+            // MovePumpkinToRandomPlace();?
+            yield return new WaitForSeconds(spawnTimeInterval); //jemtodo this is probably where I change the intervals of spawning to vary for difficulty?
+        }
+    }
+
+    private void EnableObjectInPool()
+    {
+
+       
+        for (int i = 0; i < molePool.Length; i++)
+        {
+            if (molePool[i].activeInHierarchy == false)
+            {
+                molePool[i].SetActive(true);
+                molePool[i].GetComponent<UpDown>().moleGoesUpAndDown(1f);
+                return;
+            }
+        }
+    }
+}
