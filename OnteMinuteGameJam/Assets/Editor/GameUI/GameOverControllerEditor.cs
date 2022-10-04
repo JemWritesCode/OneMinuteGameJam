eea3f33f2acca0d8033f8a6f93bc5a355a92a142
redseiko <redseiko@users.noreply.github.com>
2022-10-03 21:14:01 -0700

    Add PostProcess Volume/Layer and GameOverController.

---
+++ OnteMinuteGameJam/Assets/Editor/GameUI/GameOverControllerEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+
+using UnityEngine;
+
+[CustomEditor(typeof(GameOverController))]
+public class GameOverControllerEditor : Editor {
+  private GameOverController _gameOverController;
+
+  public void OnEnable() {
+    _gameOverController = Selection.activeGameObject.GetComponent<GameOverController>();
+  }
+
+  public override void OnInspectorGUI() {
+    base.OnInspectorGUI();
+
+    EditorGUI.BeginDisabledGroup(!Application.isPlaying);
+
+    EditorGUILayout.Space(25f);
+    ShowHideGameOverGUI();
+
+    EditorGUI.EndDisabledGroup();
+  }
+
+  private void ShowHideGameOverGUI() {
+    EditorGUILayout.BeginHorizontal();
+
+    if (GUILayout.Button("ShowGameOver")) {
+      _gameOverController.ShowGameOver();
+    }
+
+    if (GUILayout.Button("HideGameOver")) {
+      _gameOverController.HideGameOver();
+    }
+
+    EditorGUILayout.EndHorizontal();
+  }
+}
---
+++ OnteMinuteGameJam/Assets/GameUI/GameOverController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class GameOverController : MonoBehaviour {
+  [field: SerializeField]
+  public PostProcessVolume CameraEffect { get; private set; }
+
+  public void ShowGameOver() {
+    CameraEffect.enabled = true;
+  }
+
+  public void HideGameOver() {
+    CameraEffect.enabled = false;
+  }
+}
