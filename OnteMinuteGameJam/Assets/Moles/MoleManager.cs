83c419c05a6b794181cfef3abce22b90f7a02121
Jem Cope <thejemmeh@gmail.com>
2022-10-04 15:56:23 -0700

    Added a tooltip and cleaning up a bit

--- OnteMinuteGameJam/Assets/Moles/MoleManager.cs
+++ OnteMinuteGameJam/Assets/Moles/MoleManager.cs
@@ -5,24 +5,13 @@ using UnityEngine;
 
 public class MoleManager : MonoBehaviour
 {
-    // for all the pumpkins in the mole list
-    // pick a random pumpkin
-    // make that random pumpkin go up and down
-
-    // nactually I need spawn ocoordinates that roughly work for all the different things that we might spawn instead
-    // and then spawn the pumpkin right there on the fly and it goes up. This will also mean less objects on the screen at once, we just spawn the pumpkins as needed
-
-    // spawner grid
-    //spawn pumpkins at random intervals
-    // there's different color pumpkins and there's a bomb pumkin that could be the pumpkin that you try to avoid
-
-    // i could rearrange the level so it's more of a square, zoom out the camera  bi tand make it spawn on coorindates instead of bespoke positioning 
-
-    // you make an object pool and you just change the position of the pumpkins each time you enable it
-    // //and it'll also reset it? or maybe hve to do the resetting manually?
+    //idea todo there's different color pumpkins and there's a bomb pumkin that could be the pumpkin that you try to avoid
 
     [SerializeField] GameObject basicMole;
-    [SerializeField] [Range(0, 50)] int poolSize = 5;
+
+    [Tooltip("There are 100 squares on the board, so that's the max.")]
+    [SerializeField] [Range(0, 100)] int poolSize = 5;
+    
     [SerializeField] [Range(0.1f, 30f)] float spawnTimeInterval = 1f;
     public List<Tiles> placeableTiles = new List<Tiles>();
 
