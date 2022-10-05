e21ae21e4a5f73943ea240e8103c2ab34f8b9d24
Jem Cope <thejemmeh@gmail.com>
2022-10-04 20:54:38 -0700

    Added splat effect spark for when you kill the mole

--- OnteMinuteGameJam/Assets/MoleDeath.cs
+++ OnteMinuteGameJam/Assets/MoleDeath.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class MoleDeath : MonoBehaviour
 {
     Tiles tileMoleIsOn;
+    [SerializeField] ParticleSystem pumpkinSplat;
     public void KillMole()
     {
+        ParticleSystem splat = Instantiate(pumpkinSplat, transform.position, Quaternion.identity);
+        Destroy(splat, .5f);
         tileMoleIsOn = GetComponent<MoleMover>().randomTile;
         AudioSource.PlayClipAtPoint(GetComponent<AudioSource>().clip, Camera.main.transform.position);
         gameObject.GetComponent<UpDown>().DeactivateMole(tileMoleIsOn);
-
     }
 }
