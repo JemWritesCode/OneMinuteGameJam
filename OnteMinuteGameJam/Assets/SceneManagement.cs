27a3fdafd8b17a697fd6a74174a91d4026b02575
redseiko <redseiko@users.noreply.github.com>
2022-10-04 19:47:25 -0700

    Modify SceneManagement to get the GameManager reference after the UIScene is loaded.
      * Added GameManager tag.

--- OnteMinuteGameJam/Assets/SceneManagement.cs
+++ OnteMinuteGameJam/Assets/SceneManagement.cs
@@ -1,14 +1,26 @@
+using System.Linq;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneManagement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start() {
-      if (SceneManager.GetSceneByName("1.5-UI").IsValid()) {
-        // Scene already loaded.
-      } else {
-        SceneManager.LoadScene("1.5-UI", LoadSceneMode.Additive);
-      }
+public class SceneManagement : MonoBehaviour {
+  private GameManager _gameManager;
+
+  void Start() {
+    Scene scene = SceneManager.GetSceneByName("1.5-UI");
+
+    if (!scene.IsValid()) {
+      scene = SceneManager.LoadScene("1.5-UI", new LoadSceneParameters(LoadSceneMode.Additive));
     }
+
+    _gameManager =
+        scene.GetRootGameObjects()
+            .Where(go => go && go.CompareTag("GameManager"))
+            .Select(go => go.GetComponent<GameManager>())
+            .FirstOrDefault();
+
+    if (!_gameManager) {
+      Debug.LogError($"Could not find GameManager after scene load.");
+    }
+  }
 }
