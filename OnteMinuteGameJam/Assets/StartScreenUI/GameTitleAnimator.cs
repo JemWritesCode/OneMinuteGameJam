ce755ad2bfcddc75a1fc4e88ce8807a4157e3dd4
redseiko <redseiko@users.noreply.github.com>
2022-10-04 17:48:56 -0700

    Overhaul StartScreen with animators.

---
+++ OnteMinuteGameJam/Assets/StartScreenUI/GameTitleAnimator.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+
+using UnityEngine;
+
+public class GameTitleAnimator : MonoBehaviour {
+  [field: SerializeField]
+  public TMPro.TMP_Text SmashingLabel { get; private set; }
+  
+  [field: SerializeField]
+  public TMPro.TMP_Text PumpkinsLabel { get; private set; }
+
+  public void Start() {
+    DOTween.Sequence()
+        .SetLink(gameObject)
+        .Insert(0f, SmashingLabel.DOFade(0f, 2f).From())
+        .Insert(0f, SmashingLabel.transform.DOLocalMoveX(-150f, 1f).SetEase(Ease.OutBack).From(true))
+        .Insert(1.5f, SmashingLabel.transform.DOPunchPosition(new Vector3(50f, 0f, 0f), 10f, 1))
+        .Insert(0.25f, PumpkinsLabel.DOFade(0f, 2f).From())
+        .Insert(0.25f, PumpkinsLabel.transform.DOLocalMoveX(150f, 1f).SetEase(Ease.OutBack).From(true))
+        .Insert(1.75f, PumpkinsLabel.transform.DOPunchPosition(new Vector3(-50f, 0f, 0f), 10f, 1));
+  }
+}
---
+++ OnteMinuteGameJam/Assets/StartScreenUI/StartLabelAnimator.cs
@@ -0,0 +1,42 @@
+﻿using DG.Tweening;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class StartLabelAnimator : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+  [field: SerializeField]
+  public Vector3 DoScaleEndValue { get; private set; } = Vector3.one;
+
+  [field: SerializeField, Min(0f)]
+  public float DoScaleDuration { get; private set; } = 0.5f;
+
+  private Tweener _onHoverTweener;
+
+  private void Start() {
+    _onHoverTweener =
+        transform
+            .DOScale(DoScaleEndValue, DoScaleDuration)
+            .SetLink(gameObject)
+            .SetAutoKill(false)
+            .Pause();
+
+    DOTween.Sequence()
+        .SetLink(gameObject)
+        .Append(transform.DOLocalMoveY(10f, 1f).SetRelative(true).SetLoops(2, LoopType.Yoyo))
+        .AppendInterval(1.5f)
+        .SetLoops(-1, LoopType.Restart);
+
+    GetComponent<TMPro.TMP_Text>()
+        .DOFade(0f, 5f).From()
+        .SetLink(gameObject);
+  }
+
+
+  public void OnPointerEnter(PointerEventData eventData) {
+    _onHoverTweener.Restart();
+  }
+
+  public void OnPointerExit(PointerEventData eventData) {
+    _onHoverTweener.SmoothRewind();
+  }
+}
